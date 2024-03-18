c40367a36083cafb8069ec4e8ab2f53c87fdadc7
mario.arista <mario.arista.dev@gmail.com>
2024-03-18 14:22:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,123 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body class="bg-dark">
+
+    <header class="container-fluid bg-secondary">
+        <h1 class="text-warning fs-1 p-3 text-center">TOVA IL TUO HOTEL</h1>
+    </header>
+    
+    <main class="container-fluid"> 
+
+        <div class="row-col-1">
+
+            <div class="col-6 m-auto pt-4">
+                <form action="index.php" method="GET">
+
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected value="-1">Cerca per parcheggio</option>
+                        <option value="1">Con Parcheggio</option>
+                        <option value="2">Senza Parcheggio</option>
+                    </select>
+
+                    <div>
+                        <label class="text-white p-2" for="parola">Cerca per voto:</label>
+                        <input class="w-100 p-2 border border-0 rounded-3" name="voto" id="voto" type="number" placeholder="3">
+                    </div>
+                    <input id="invia" class="text-dark bg-warning p-2 w-100 border border-0 rounded-3 mt-3" type="submit" label="INVIA">
+                </form>
+            </div>
+
+            <div class="col-6 m-auto pt-4">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+
+
+     <!-- BOOTSTRAP -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
