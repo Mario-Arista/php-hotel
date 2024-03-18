e8b5736eb94e93344c003a33c9f2aa9cc4d0b8f2
mario.arista <mario.arista.dev@gmail.com>
2024-03-18 16:24:19 +0100

    Bonus 1 + bonus 3

--- index.php
+++ index.php
@@ -40,7 +40,29 @@
 
     ];
 
-    $filteredHotels = [];
+    // Filtraggio degli hotel in base alla select
+    if (isset($_GET['parcheggio'])) {
+
+        $filteredHotels = [];
+
+        foreach ($hotels as $hotel) {
+
+            if ($_GET['parcheggio'] == 2 && $hotel['parking'] == true) {
+
+                $filteredHotels[] = $hotel;
+
+            } else if ($_GET['parcheggio'] == 3 && $hotel['parking'] == false) {
+                $filteredHotels[] = $hotel;
+
+            } else if ($_GET['parcheggio'] == 1) {
+                $filteredHotels = $hotels;
+            }
+        }
+
+    } else {
+
+        $filteredHotels = $hotels;
+    }
 
 ?>
 
@@ -69,18 +91,21 @@
             <div class="col-6 m-auto pt-4">
                 <form action="index.php" method="GET">
 
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected value="-1">Cerca per parcheggio</option>
-                        <option value="1">Con Parcheggio</option>
-                        <option value="2">Senza Parcheggio</option>
+                    <select class="form-select" name="parcheggio" aria-label="Default select example">
+                        <option selected value="">Filtra per parcheggio</option>
+                        <option value="1">Tutti gli Hotel</option>
+                        <option value="2">Con Parcheggio</option>
+                        <option value="3">Senza Parcheggio</option>
                     </select>
 
-                    <div>
-                        <label class="text-white p-2" for="parola">Cerca per voto:</label>
-                        <input class="w-100 p-2 border border-0 rounded-3" name="voto" id="voto" type="number" placeholder="3">
-                    </div>
+                    <!-- <div>
+                        <label class="text-white p-2" for="parola">Cerca per voto (es. almeno 2):</label>
+                        <input class="w-100 p-2 border border-0 rounded-3" name="voto" id="voto" type="number" placeholder="2">
+                    </div> -->
+
                     <input id="invia" class="text-dark bg-warning p-2 w-100 border border-0 rounded-3 mt-3" type="submit" label="INVIA">
                 </form>
+
             </div>
 
             <div class="col-6 m-auto mt-5">
@@ -89,7 +114,8 @@
                         <tr>
                             <?php 
                                 foreach ($hotels[0] as $key => $value) {
-                                    echo "<th>$key</th>";
+                                    $formatted_key = str_replace('_', ' ', strtoupper($key));
+                                    echo "<th>$formatted_key</th>";
                                 }
                             ?>
                         </tr>
@@ -97,7 +123,8 @@
 
                     <tbody>
                         <?php 
-                            foreach ($hotels as $currentHotel) {
+                            foreach ($filteredHotels as $currentHotel) {
+
                                 echo "<tr>";
                                 foreach ($currentHotel as $key => $value) {
                                     if ($key == 'parking') {
